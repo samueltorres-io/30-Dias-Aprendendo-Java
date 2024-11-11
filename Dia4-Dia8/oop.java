4aefed933a37f2598fdcc8ef4e2d90aeea81380e
samueltorres-io <torresfernandessamuel@gmail.com>
2024-11-11 14:25:16 -0300

    aula construtores em java
    cosntrutores

--- Dia4-Dia8/oop.java
+++ Dia4-Dia8/oop.java
@@ -25,3 +25,26 @@ public class Main {
 // public = so pode ser acessado por objetos.
 
 
+// Construtor
+
+public class Main {
+    int x;
+    
+    // Construtor da classe
+    public Main() {
+        x = 5;
+    }
+    
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+    }
+    
+}
+
+// O nome do costrutor e correspondente ao nome da classe;
+// O construtor e chamado quando um novo objeto e criado: Main();
+// Toda classe tem um construtor! Se não definirmos um, o java cria por padrao, mas nao conseguiremos definir valores iniciais para o objeto.
+
+
+
